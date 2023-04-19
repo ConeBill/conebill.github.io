381f7d54e90738db867b3114fb79a98bf6fc82ea
ConeBill <paulohvsv@gmail.com>
2023-04-19 09:22:41 -0300

    homo1

---
+++ assects/js/construcao.js
@@ -0,0 +1,7 @@
+const areaTrabalho = document.getElementsByClassName('area');
+
+export function att (obj, msg) {
+    if(obj & msg != null) {
+        const novaArea = document.createElement('Em obras')
+    }
+}
